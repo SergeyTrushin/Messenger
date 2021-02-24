0b4e3eabb6f5f28adebd65d4fe6746d4d7182aa2
Sergey <sergeytrushin-17@yandex.ru>
2021-02-24 14:47:10 +0400

    init nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+}
+</style>
